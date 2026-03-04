355bb2d58878a5448b83418a07d44d420cf36c54
Puyang Wang <pauliwang411@gmail.com>
2026-03-03 21:16:56 -0800

    Delete scripts used for checking Niivue colormaps.

--- check_colormaps.js
+++
@@ -1,3 +0,0 @@
-const { Niivue } = require('@niivue/niivue');
-const nv = new Niivue();
-console.log(nv.colormaps());
--- project/check_colormaps.ts
+++
@@ -1,3 +0,0 @@
-import { Niivue } from '@niivue/niivue';
-const nv = new Niivue();
-console.log(nv.colormaps());
